d725dceaff3457f603e57867ce34da3f959f8330
Wenchong Lai <wenchonglai@gmail.com>
2021-07-23 23:18:58 -0700

    Add 20210723 daily practice exercise

---
+++ sidetrack/20210723-814-binary-tree-pruning.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+      if not root:
+          return None
+
+      root.left = self.pruneTree(root.left)
+      root.right = self.pruneTree(root.right)
+
+      if not root.left and not root.right:
+        return None if root.val == 0 else root
+
+      return root
